78a68bd8a17a388e77aed9d8165b23f6956ddb2f
IrainGao <2661626900@qq.com>
2025-11-03 23:05:31 +0800

    gy update

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>恭喜，站点创建成功！</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+
+        .container {
+            width: 60%;
+            margin: 10% auto 0;
+            background-color: #f0f0f0;
+            padding: 2% 5%;
+            border-radius: 10px
+        }
+
+        ul {
+            padding-left: 20px;
+        }
+
+            ul li {
+                line-height: 2.3
+            }
+
+        a {
+            color: #20a53a
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>恭喜, 站点创建成功！</h1>
+        <h3>这是您的测试页面</h3>
+    </div>
+</body>
+</html>
